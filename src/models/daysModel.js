ea08f6d3b293d3e0e4a51b41a871205c3596f277
didier-snake <didier-snake@didier-snakes-MacBook-Pro.local>
2026-04-04 07:53:13 +0100

    fix day model

--- src/models/daysModel.js
+++ src/models/daysModel.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 const Day = (sequelize) => {
-  return sequelize.define("Message", {
+  return sequelize.define("Day", {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
